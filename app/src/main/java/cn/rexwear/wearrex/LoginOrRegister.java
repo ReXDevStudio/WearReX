ba215594a560dc348ac9db1b5988f01817258cba
Liushilang <1460499332@qq.com>
2022-02-07 11:15:17 +0800

    added register fragment; little fix-up

--- app/src/main/java/cn/rexwear/wearrex/LoginOrRegister.java
+++ app/src/main/java/cn/rexwear/wearrex/LoginOrRegister.java
@@ -13,11 +13,11 @@ import android.view.ViewGroup;
 import android.widget.ImageButton;
 
 
-public class LoginOrUp extends Fragment {
+public class LoginOrRegister extends Fragment {
     ImageButton login;
     ImageButton logup;
 
-    public LoginOrUp() {
+    public LoginOrRegister() {
         // Required empty public constructor
     }
 
@@ -31,7 +31,7 @@ public class LoginOrUp extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login_or_up, container, false);
+        return inflater.inflate(R.layout.fragment_login_or_register, container, false);
     }
 
     @Override
